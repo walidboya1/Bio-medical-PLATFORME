f578c17bb7edfaf596a1be511808739150ad3c64
_NULL <fahd.adni@gmail.com>
2020-04-19 22:21:20 +0000

    Update fichepat.php

--- fichepat.php
+++ fichepat.php
@@ -1,15 +1,8 @@
-
 <?php
-  error_reporting(0);
-  include 'config.php';
-  session_start();
-  if (empty($_SESSION['login_user'])){
-    header("location: index.php");
-    exit();
-  }
-  
-
-
+	require 'required.php';
+	istechnicien();
+	$usern=$_GET['user'];
+	isuser($usern);
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,13 +12,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="keywords" content="">
+
  <!-- Styles -->
     <link href="assets/css/core.min.css" rel="stylesheet">
     <link href="assets/css/thesaas.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
+
     <!-- Favicons -->
     <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
     <link rel="icon" href="assets/img/favicon.png">
+
   <title>TELERADIO - Technicien</title>
 </head>
 <body>
@@ -34,6 +30,7 @@
       | Topbar
       |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
       !-->
+
       <nav class="topbar topbar-expand-md topbar-nav-centered topbar-inverse topbar-sticky">
         <div class="container">
           
@@ -44,25 +41,40 @@
               <img class="logo-inverse" src="assets/img/logo.png" alt="logo">
             </a>
           </div>
+
+
+
+
           <div class="topbar-right">
             <a class="btn btn-sm btn-danger mr-4" href="index.php">Se deconnecter</a>
           </div>
+
         </div>
       </nav>
       <!---- END topbar---->
+
+
     <!-- Header -->
     <header class="header header-inverse h-fullscreen">
       <div class="header-overlay opacity-90" style="background-color:   #F5BD1f"></div>
+
       <div class="container text-center">
+
         <div class="row h-full">
           <div class="col-12 col-lg-8 offset-lg-2 align-self-center">
+
             <h5 class="display-4">Fiche Patient</h5>
            
           </div>
         </div>
+
       </div>
     </header>
     <!-- END Header -->
+
+
+
+
        <!--
       |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
       | Apply form
@@ -75,10 +87,13 @@
             <hr>
             <p class="lead">Veillez Remplir la fiche du patient s'il vous plaît.</p>
           </header>
+
+
           <div class="row">
             <div class="col-12 col-md-8 offset-md-2">
               
-<form method="post" enctype="multipart/form-data">
+              <form action="#" method="POST">
+
                 <div class="row">
                   <div class="form-group col-12 col-md-6">
                       <input class="form-control" type="text" name="nom" id="nom" placeholder="Nom">
@@ -95,95 +110,59 @@
                   <div class="form-group col-12 col-md-6">
                     <input class="form-control" type="age" placeholder="Age" name="age" id="age">
                   </div>
+
                   <div class="form-group col-12 col-md-6">
                     <input class="form-control" type="phone" placeholder="Telephone" name="tele" id="tele">
                   </div>
                 </div>
 
-                  
+                
+
+                <div class="form-group input-group file-group">
+                  <input type="text" class="form-control file-value" name="files" id="files" placeholder="Choisir fichier..." readonly>
+                  <input type="file" multiple>
+                  <span class="input-group-btn">
+                    <button class="btn btn-white file-browser" type="button"><i class="fa fa-upload"></i></button>
+                  </span>
+                </div>
+
                 <div class="row">
                   <div class="form-group col-12 col-md-6">
                   <input class="form-control" type="text" placeholder="Type" name="type" id="type">
                 </div>
+
                   <div class="form-group col-12 col-md-6">
                   <input class="form-control" type="date" placeholder="Date" name="date" id="date">
                 </div>
               </div>
 
-                 <input class="btn btn-outline btn-primary w-full mb-10"   type="File" name="file[]" id="file" multiple></input>
-                    <input class="btn btn-primary btn-block" type="submit" name="submit" value = "upload" onclick="javascript:ShowHelp()">
+                
+
+                <button class="btn btn-primary btn-block" type="submit">Ajouter Patient</button>
               </form>
+
             </div>
           </div>
+
+
         </div>
       </section>
+
 <!--
       |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
       | END Apply form
       |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
       !-->
+
+
+
+
+
+
     <!-- Scripts -->
     <script src="assets/js/core.min.js"></script>
     <script src="assets/js/thesaas.min.js"></script>
     <script src="assets/js/script.js"></script>
 
-     <script>
-    function ShowHelp()
-{ alert("USER ADDED SUCCESFULLY !!!!");
-}</script>
-
 </body>
 </html>
-
- 
-<?php 
-    include 'config.php';
-if (isset($_POST["submit"]))
- {
-     
-
-    $nom = $_POST['nom'];
-    $sexe = $_POST['sexe'];
-    $age = $_POST['age'];
-    $tele = $_POST['tele'];
-    $type = $_POST['type'];
-    $date = $_POST['date'];
-
-
-                  $deg_query1 = "SELECT id from technitien where id_user ='".$_SESSION['login_user']."'";
-$deg_table1 = mysqli_query($connection,$deg_query1);
-if($deg_table1){
-                $deg_column1=mysqli_num_rows($deg_table1);
-                if($deg_column1 > 0){
-                      $deg_column1 = mysqli_fetch_assoc($deg_table1);
-$query = "INSERT INTO patient(id_technicien,age,sexe,nom,tele) values('".$deg_column1['id']."','$age','$sexe','$nom','$tele')";
-$table = mysqli_query($connection,$query);
-
-}}
-
-
-$query1 = "SELECT id from patient where nom = '$nom' and  id_technicien = '".$deg_column1['id']."'";
-$table1 = mysqli_query($connection,$query1);
-
-$row=mysqli_num_rows($table1);
-if($row > 0){
-                    $row = mysqli_fetch_assoc($table1);
-
-$filecount = count($_FILES['file']['name']);
-for($i=0;$i<$filecount;$i++){
-    $pname = $_FILES['file']['name'][$i];
-
-    #sql query to insert into database
-    $sql = "INSERT into image(Radio,Date,Type,Id_patient) values ('$pname','$date','$type','".$row['id']."')"; 
- 
-    if(mysqli_query($connection,$sql)){
-  }
-  
-move_uploaded_file($_FILES['file']['tmp_name'][$i], 'upload/'.$pname);
-
-}}}
-
- mysql_close($connection);
- 
-?>
-
