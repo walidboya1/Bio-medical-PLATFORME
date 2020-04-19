5186cd1b9793afa95012f8829517c77a254d44f9
_NULL <fahd.adni@gmail.com>
2020-04-19 18:27:27 +0000

    Fixed UPDATE problem
    Fixed update query using the new added function

--- login.php
+++ login.php
@@ -1,11 +1,10 @@
 <?php
-	error_reporting(0);
+
 	include 'config.php';
 	session_start(); // Starting Session
-	$error=''; // Variable To Store Error Message
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		if (empty($_POST['log_email']) || empty($_POST['log_passwd'])) {
-			$error = "Username or Password is empty";
+			echo "Username or Password is empty";
 		}
 		else {
 			$email = $_POST['log_email'];
@@ -13,13 +12,10 @@
 
 			$email = stripslashes($email);
 			$passwd = stripslashes($passwd);
-			$email = mysql_real_escape_string($email);
-			$passwd = mysql_real_escape_string($passwd);
 			$passwd = md5($passwd);
 
 			// SQL query to fetch information of registerd users and finds user match.
-			$query = "CALL Login('$email','$passwd')";
-
+			$query = sprintf("CALL Login('%s','%s')",$email,$passwd);
 			$table = mysqli_query($connection,$query);
 
 			if($table){
@@ -30,21 +26,22 @@
 					$str=rand(); 
 					$result = md5($str); 
 					$_SESSION['login_user'] = $row['id'];
-					$_SESSION['user_name'] = $row['user_name'];
 					$_SESSION['email_id'] = $row['email_id'];
-					$_SESSION['admin'] = $row['prof'];
-					$_SESSION['dp_url'] = $row['img_url'];
 					$_SESSION['User_Id']=$result;
-					setcookie("dp_url", $_SESSION['dp_url']);
-					setcookie("user_name", $_SESSION['user_name']);
-					$query="UPDATE user set session='".$result."' where id=".$_SESSION['login_user'];
-					$sess=mysqli_query($connection,$query);
+					$query=sprintf("UPDATE user set session='%s' where id=%d;",$result,$_SESSION['login_user']);
+					clearStoredResults();
+					if (mysqli_query($connection,$query)) {
+						mysqli_close($connection);
+						header("location: index.php");
+					} else {
+						    echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+					}
+				}
 				else {
 					$error = "Username or Password is invalid";
+					echo $error;
 				}
-			} 
-			mysql_close($connection); // Closing Connection
+			}
 		}
-		header("location: page-login.php?error=$error"); // Redirecting To Other Page
 	}
 ?>
